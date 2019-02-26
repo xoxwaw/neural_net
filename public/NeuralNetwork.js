48b282684fe8eca313a102e9732890bbc90cfee7
hungphi98 <nguyenhungphi2698@Gmail.com>
2019-02-25 21:07:03 -0500

    neural networks with 3 layers for logistic regression

--- public/NeuralNetwork.js
+++ public/NeuralNetwork.js
@@ -1,12 +1,23 @@
+// var assert = require('assert');
 class NeuralNetwork {
     constructor(x, y) {
         this.input = x;
-        this.weights1 = this.generateRandomMatrix(x.length,x[0].length);//weights
+        this.training_size = y.length;
+        this.weights1 = this.generateRandomMatrix(3,x[0].length);//weights
+        this.biases1 = this.generateRandomMatrix(3,1);
+        this.weights2 = this.generateRandomMatrix(2,this.weights1.length);
+        this.biases2 = this.generateRandomMatrix(2,1);
+        this.weights3 = this.generateRandomMatrix(1,this.weights2.length);
+        this.biases3 = this.generateRandomMatrix(1,1);
+        this.z_1 = this.generateRandomMatrix(3,1);
+        this.a_1 = this.generateRandomMatrix(3,1);
+        this.z_2 = this.generateRandomMatrix(2,1);
+        this.a_2 = this.generateRandomMatrix(2,1);
+        this.z_3 = this.generateRandomMatrix(1,1);
+        this.a_3 = this.generateRandomMatrix(1,1);
         this.y = y; //actual output
         this.output = this.generateZeros(y.length); //initialize predicted output
         this.alpha = 0.1; //learning rate
-        this.layer1 = [];//hidden layer 1
-        this.passforward = [];
     }
     generateRandomMatrix(row, col) {
         var matrix = [];
@@ -25,106 +36,128 @@ class NeuralNetwork {
         }
         return vector;
     }
-    sigmoid(x) {
+    _sigmoid(x) {
         return 1 / (1 + Math.pow(Math.E, -x))
     }
-    sigmoidDerivative(x) {
-        return this.sigmoid(x) * (1 - this.sigmoid(x));
-    }
-    dot(m1, m2) {
-        var row = m1.length;
-        var col = m1[0].length;
-        var result = [];
-        for (var i = 0; i < row; i++){
-            result[i] = 0;
-            for (var j = 0; j < col; j++){
-                result[i] += m1[i][j] * m2[i][j];
-            }
-        }
-        return result;
-    }
-    dotVectorVector(v1,v2){
-        var res = 0;
-        for (var i = 0; i < v1.length; i++){
-            res += v1[i] * v2[i];
-        }
-        return res;
-    }
-    dotVectorMatrix(v,m){
+    sigmoid(m){
         var res = [];
         for (var i = 0; i < m.length; i++){
             res[i] = [];
-            for (var j = 0; j < m[0].length; j++){
-                res[i][j]= m[i][j] * v[i];
+            for (var j = 0; j < m[0].length;j++){
+                res[i][j] = this._sigmoid(m[i][j]);
             }
         }
         return res;
     }
-    add(m1,m2){
-        var result = [];
-        var row = m1.length;
-        var col = m1[0].length;
-        for (var i = 0; i < row; i++){
-            result[i] = [];
-            for (var j = 0 ; j < col; j ++){
-                result[i][j] = m1[i][j] + m2[i][j];
-            }
-
-        }
-        return result;
+    _sigmoidDerivative(x) {
+        return this._sigmoid(x) * (1 - this._sigmoid(x));
     }
-    substractVector(v1,v2){
-        var result = [];
-        var col = v1.length;
-        for (var i = 0; i < col; i++){
-            result[i] = v1[i] - v2[i];
-        }
-        return result;
-    }
-    substractMatrix(m1,m2){
+    sigmoidDerivative(m){
         var res = [];
+        for (var i = 0; i < m.length; i++){
+            res[i] = [];
+            for (var j = 0; j < m[0].length;j++){
+                res[i][j] = this._sigmoidDerivative(m[i][j]);
+            }
+        }
+        return res;
+    }
+    sumOf(m){
+        var res = 0;
+        for (var i = 0; i < m.length; i++){
+            for (var j = 0; j < m[0].length;j++){
+                res += m[i][j];
+            }
+        }
+        return res;
+    }
+    add(m1,m2,operator){
+        var res= [];
+        if (m1.length != m2.length){
+            console.log(m1.length,m2.length);
+            return [];
+        }
+        for (var i = 0; i < m1.length;i++){
+            res[i] = []
+            for (var j = 0; j < m1[0].length;j++){
+                if (operator == "+") res[i][j] = m1[i][j] + m2[i][j];
+                else if (operator == "-") res[i][j] = m1[i][j] - m2[i][j];
+            }
+        }
+        return res;
+    }
+
+    scalarOp(m, num, op){
+        var res = [];
+        for (var i = 0; i < m.length; i++){
+            res[i] = [];
+            for (var j = 0; j < m[0].length;j++){
+                if (op == "*") res[i][j] = m[i][j] * num;
+                else if (op == "+") res[i][j] = m[i][j] + num;
+                else if (op == "-") res[i][j] = m[i][j] - num;
+            }
+        }
+        return res;
+    }
+    elemProduct(m1,m2){
+        var res = [];
+        if (m1.length != m2.length || m1[0].length != m2[0].length){
+            console.log(m1.length, m2.length, m1[0].length, m2[0].length);
+        }
         for (var i = 0; i < m1.length; i++){
             res[i] = [];
             for (var j = 0; j < m1[0].length; j++){
-                res[i][j] = m1[i][j] - m2[i][j];
+                res[i][j] = m1[i][j] * m2[i][j];
             }
         }
         return res;
     }
-    multiplyVector(v, num){
-        var new_v = [];
-        for (var i = 0; i < v.length; i++){
-            new_v[i] = num * v[i];
-        }
-        return new_v;
-    }
-    multiplyMatrix(m, num){
-        var new_m = [];
-        for (var i = 0; i < m.length; i++){
-            new_m[i] = [];
-            for (var j = 0; j < m.length; j++){
-                new_m[i][j] = m[i][j] * num;
+    dot(m1,m2){
+        // assert(m1.length == m2.length);
+
+        var res = [];
+        for (var i = 0; i < m1.length; i++){
+            res[i] = [];
+            for (var j = 0; j < m2.length; j++){
+                res[i][j] = 0;
+                for (var k = 0; k < m1[0].length;k++){
+                    res[i][j] += m1[i][k] * m2[j][k];
+                }
             }
         }
-        return new_m
+        return res;
     }
-    feedForward() {
-        this.passforward = this.dot(this.input, this.weights1); //z = x.W
-        for (var i = 0; i < this.y.length; i++){
-            this.layer1[i] = this.sigmoid(this.passforward[i]); //a[1] = sigmoid(z)
-        }
-        this.output = this.layer1;
-        return this.output;
+
+    feedForward(index) {
+        this.z_1 = this.add(this.dot(this.weights1, [this.input[index]]), this.biases1, "+");
+        this.a_1 = this.sigmoid(this.z_1);
+        this.z_2 = this.add(this.dot(this.weights2, nn.transpose(this.a_1)), this.biases2, "+");
+        this.a_2 = this.sigmoid(this.z_2);
+        this.z_3 = this.add(this.dot(this.weights3, nn.transpose(this.a_2)), this.biases3, "+");
+        this.a_3 = this.sigmoid(this.z_3);
+        return this.a_3;
     }
-    backpropagate() {
-        var error = this.multiplyVector(this.substractVector(this.y, this.output),-2);//error = -2(y - (x.W))
-        var d_yw = [];
-        for (var i = 0; i < this.passforward.length; i++){
-            d_yw[i] = this.sigmoidDerivative(this.passforward[i]); //d_yw = d_sigmoid(z)
-        }
-        var d_weight = this.dotVectorMatrix(error, this.input);// dE/dW = x . error
-        d_weight = this.multiplyMatrix(this.dotVectorMatrix(d_yw, d_weight), this.alpha);//alpha * d_yw
-        this.weights1 = this.substractMatrix(this.weights1,d_weight);// modify weight
+    backpropagate(ind) {
+        var d_z_3 = [[this.a_3 - this.y[ind]]];
+        var d_W_3 = this.scalarOp(this.dot(d_z_3, this.a_2), 1 / this.training_size, "*") ;
+        var d_b_3 = this.sumOf(d_z_3) / this.training_size;
+
+        var d_z_2 = this.elemProduct(this.dot(this.transpose(this.weights3), d_z_3), this.sigmoidDerivative(this.z_2));
+        var d_W_2 = this.scalarOp(this.dot(d_z_2, this.a_1), 1 / this.training_size, "*");
+        var d_b_2 = this.sumOf(d_z_2) / this.training_size;
+
+        var d_z_1 = this.elemProduct(this.dot(this.transpose(this.weights2), this.transpose(d_z_2)), this.sigmoidDerivative(this.z_1));
+        var d_W_1 = this.scalarOp(this.dot(d_z_1, this.transpose([this.input[ind]])), 1 / this.training_size, "*");
+        var d_b_1 = this.sumOf(d_z_1) / this.training_size;
+
+        this.weights1 = this.add(this.weights1, this.scalarOp(d_W_1, this.alpha, "*"), "-");
+        // console.log(this.weights1);
+        this.weights2 = this.add(this.weights2, this.scalarOp(d_W_2, this.alpha, "*"), "-");
+        this.weights3 = this.add(this.weights3, this.scalarOp(d_W_3, this.alpha, "*"), "-");
+
+        this.biases1 = this.scalarOp(this.biases1, d_b_1, "-");
+        this.biases2 = this.scalarOp(this.biases2, d_b_2, "-");
+        this.biases3 = this.scalarOp(this.biases3, d_b_3, "-");
 
     }
     transpose(matrix) {
@@ -151,11 +184,18 @@ class NeuralNetwork {
     }
     predict(x){
         this.input = x;
-        this.feedForward();
-        return this.output;
+        var res = [];
+        for (var i = 0; i< x.length; i++){
+            res.push(this.feedForward(i)[0][0]);
+        }
+        return res;
+
     }
     train() {
-        this.feedForward();//
-        this.backpropagate();
+        for (var i = 0; i < this.training_size; i++){
+            this.feedForward(i);//
+            this.backpropagate(i);
+        }
+
     }
 }
